e4dd29560127a8c948db4c8e2e80d48d7892cead
CeleX <tachibanakanade2003@gmail.com>
2023-12-17 21:50:08 +0700

    finish landing page

--- src/scripts/responsive.js
+++ src/scripts/responsive.js
@@ -1,4 +1,25 @@
-// Thay đổi hamburger icon khi responsive heaader
+let isHamburgerOpen = false;
+
+// Handle hamburger khi chuyển qua lại giữa các kích thước màn hình
+function handleHamburger() {
+    let hamburger = document.querySelector(".lucide-menu");
+    let close = document.querySelector(".lucide-x");
+
+    if (window.innerWidth >= 1024) {
+        hamburger.style.display = "none";
+        close.style.display = "none";
+    } else if (window.innerWidth < 1024 && isHamburgerOpen == true) {
+        hamburger.style.display = "none";
+        close.style.display = "block";
+    } else if (window.innerWidth < 1024 && isHamburgerOpen == false) {
+        hamburger.style.display = "block";
+        close.style.display = "none";
+    }
+}
+window.addEventListener("resize", handleHamburger);
+handleHamburger();
+
+// Thay đổi hamburger icon khi responsive header
 function onClickHamburger(e) {
     let hamburger = document.querySelector(".lucide-menu");
     let close = document.querySelector(".lucide-x");
@@ -9,10 +30,12 @@ function onClickHamburger(e) {
         close.style.display = "block";
         navBarItems.classList.remove("hidden");
         navBarItems.classList.add("flex");
+        isHamburgerOpen = true;
     } else {
         hamburger.style.display = "block";
         close.style.display = "none";
         navBarItems.classList.remove("flex");
         navBarItems.classList.add("hidden");
+        isHamburgerOpen = false;
     }
 }
